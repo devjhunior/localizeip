449992674164b55bd1e88b088ddb209b8d9ef08a
M N Junior <marionascimentojunior@hotmail.com>
2021-05-16 23:49:26 -0300

    adding locateip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.geoloc.services import Service
 
 app = FastAPI()
 
@@ -6,6 +7,14 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {"Localization": "Internet Protocol"}
+
+
+@app.post('/locateip')
+async def locIP(ip):
+    res = Service()
+    loc = res.get_geoloc(ip)
+
+    return loc
 
 
 if __name__ == '__main__':
