2a39d73089c080a451409768dd3f0e1a29530308
M N Junior <marionascimentojunior@hotmail.com>
2021-05-16 23:46:28 -0300

    improved Procfile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-# from app.geoloc.services import Service
 
 app = FastAPI()
 
@@ -7,14 +6,6 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {"Localization": "Internet Protocol"}
-
-
-# @app.post('/locateip')
-# async def locIP(ip):
-#     res = Service()
-#     loc = res.get_geoloc(ip)
-
-#     return loc
 
 
 if __name__ == '__main__':
